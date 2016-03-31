c31b103776920d5a302e0c576eede03f3bc8a9f7
Jakub Dorňák <jdornak@redhat.com>
2016-03-31 17:09:49 +0200

    setup.py compatible with pip

--- README.md
+++ README.md
@@ -3,3 +3,39 @@ Leprikón
 
 Leprikón je informační systém pro střediska volného času,
 domy dětí a mládeže a podobné organizace.
+
+Instalace
+---------
+
+Vytvoření a aktivace virtuálního prostředí:
+```shell
+virtualenv env
+. env/bin/activate
+```
+
+Instalace Leprikónu včetně závislostí:
+```shell
+pip install https://github.com/leprikon-cz/leprikon/archive/master.zip
+```
+
+Vytvoření projektu:
+```shell
+django-admin startproject myddm --template=https://github.com/leprikon-cz/project_template/archive/master.zip
+cd myddm
+```
+
+Inicializace databáze:
+```shell
+./manage.py migrate
+```
+
+Vytvoření uživatele:
+```shell
+./manage.py createsuperuser
+```
+
+Vytvoření uživatele:
+```shell
+./manage.py runserver
+```
+
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
@@ -15,6 +16,17 @@ setup(
     license         = 'BSD',
     url             = 'https://github.com/leprikon-cz/leprikon',
     packages        = find_packages(),
+    include_package_data = True,
+    install_requires=[
+        'django>=1.7',
+        'django-cms>=3.2',
+        'djangocms_text_ckeditor',
+        'django-filer',
+        'cmsplugin-filer',
+        'django-localflavor',
+        'xhtml2pdf',
+        'django-ganalytics',
+    ],
     classifiers     = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
