a004d95a7a42193dd5885657ea76141e88e91e5f
Jakub Dorňák <jdornak@redhat.com>
2016-04-24 21:13:55 +0200

    fixed and automated date pickers

--- leprikon/forms/form.py
+++ leprikon/forms/form.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
 
+from django.forms.fields import DateField
 
 class FormMixin(object):
     required_css_class = 'required'
@@ -7,5 +8,11 @@ class FormMixin(object):
     def __init__(self, *args, **kwargs):
         super(FormMixin, self).__init__(*args, **kwargs)
         for f in self.fields:
-            self.fields[f].widget.attrs = {'class': 'form-control'}
+            # add form-control to classes
+            classes = set(self.fields[f].widget.attrs.get('class','').split())
+            classes.add('form-control')
+            self.fields[f].widget.attrs['class'] = ' '.join(classes)
+            # add data-type
+            if 'data-type' not in self.fields[f].widget.attrs:
+                self.fields[f].widget.attrs['data-type'] = self.fields[f].__class__.__name__
 
