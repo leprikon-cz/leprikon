9ca662b85eabad57a49e62e696a3a548870e8bc9
Jakub Dorňák <jdornak@redhat.com>
2016-07-05 16:23:08 +0200

    cms_toolbar

---
+++ leprikon/cms_toolbars.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+
+from cms.cms_toolbars import ADMIN_MENU_IDENTIFIER
+from cms.toolbar.items import SubMenu
+from cms.toolbar_base import CMSToolbar
+from cms.toolbar_pool import toolbar_pool
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+
+@toolbar_pool.register
+class LeprikonToolbar(CMSToolbar):
+
+    def populate(self):
+        # Articles item in main menu
+        admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER)
+        position = admin_menu.get_alphabetical_insert_position(_('Leprikon'), SubMenu)
+        url = reverse('admin:app_list', args=('leprikon',))
+        admin_menu.add_sideframe_item(_('Leprikon'), url=url, position=position)
+
