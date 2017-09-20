e9ad8e59004299c486a899b68a203e9b71ef1c52
Jakub Dorňák <jakub.dornak@misli.cz>
2017-09-21 00:54:14 +0200

    gettext overrides

---
+++ leprikon/gettext_overrides.py
@@ -0,0 +1,9 @@
+from django.utils.translation import ugettext_lazy as _
+
+'''
+    This module contains strings from other packages, which are not correctly
+    translated to Czech language.
+'''
+
+# cms
+_('Publish page changes')
