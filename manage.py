fa7c5dc3cf9a1a3b19ae773951a993debfb36002
Jakub Dorňák <jakub.dornak@misli.cz>
2017-10-30 09:40:57 +0100

    fake manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,23 @@
-bin/leprikon
+#!/bin/bash
+
+if ! which leprikon &> /dev/null; then
+    if ! test -e env/bin/activate; then
+        virtualenv env
+    fi
+    . env/bin/activate
+    if ! which leprikon &> /dev/null; then
+        pip install -r requirements.txt
+    fi
+fi
+
+if [ -n "$1" ]; then
+    export DEBUG="${DEBUG:-TEMPLATE}"
+else
+    # bash-completion
+    export DEBUG=""
+fi
+
+export PYTHONPATH=.
+#export SITE_MODULE=leprikon
+
+exec leprikon "$@"
