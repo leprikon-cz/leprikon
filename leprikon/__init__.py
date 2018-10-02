f2e9710f904b1f605bb98808d8760343202a8046
Jakub Dorňák <jakub.dornak@misli.cz>
2018-10-02 18:13:18 +0200

    Bump version: 1.12.2 → 1.12.3

--- leprikon/__init__.py
+++ leprikon/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.12.2'
+__version__ = '1.12.3'
 default_app_config = 'leprikon.apps.LeprikonConfig'
 
 staticfiles_urls = {
@@ -24,7 +24,7 @@ staticfiles_urls = {
     'leprikon/lib/bootstrap-multiselect.min.js': ('https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.min.js', 'sha384', '2q+7Pk3OSOseTKfNi/t8mZzYRIovdg+nfXYeKmmnivkyD+nxosintmGKX7SWcrOG'),  # NOQA
     'leprikon/lib/ekko-lightbox.min.css': ('https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/4.0.1/ekko-lightbox.min.css', 'sha384', 'EsoR0pjPTZTUy5b8kJf6aTfA7quYluD5mNQ/Lz4hsxU2kXuuc6PlXVfBJCFdTXqD'),  # NOQA
     'leprikon/lib/ekko-lightbox.min.js': ('https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/4.0.1/ekko-lightbox.min.js', 'sha384', '2aVpK/uKiYlyFXHZ36wvxOPZyWeouEKvbSUlqCrkFhyT80n2aSYtT15Tgdn6g41M'),  # NOQA
-    'leprikon/lib/jquery.min.js': ('https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.2/jquery.min.js', 'sha384', 'o6l2EXLcx4A+q7ls2O2OP2Lb2W7iBgOsYvuuRI6G+Efbjbk6J4xbirJpHZZoHbfs'),  # NOQA
+    'leprikon/lib/jquery.min.js': ('https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.3/jquery.min.js', 'sha384', 'o6l2EXLcx4A+q7ls2O2OP2Lb2W7iBgOsYvuuRI6G+Efbjbk6J4xbirJpHZZoHbfs'),  # NOQA
     'leprikon/lib/moment-with-locales.min.js': ('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment-with-locales.min.js', 'sha384', 'V0utPzNT28o7j37Ll4BIKqEVcL1yOE45HoCoaLu/CqF4nFaf2nAyZTHKJW1KdJMH'),  # NOQA
     'leprikon/lib/bootstrap-social.min.css': ('https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css', 'sha384', 'N7gull5mxE2uLDqZ+kcZolhmuopwLtTcThUsVnUS2LVMQljojN9cfJ9A6fUA63zB'),  # NOQA
     'leprikon/lib/font-awesome/css/font-awesome.css': 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css',  # NOQA 
