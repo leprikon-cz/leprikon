b9b35723ae997a5d18d5b1b9da46a507d40c6e7d
Jakub Dorňák <jakub.dornak@misli.cz>
2019-08-11 10:05:23 +0200

    fix social auth pipeline to associate by email

--- leprikon/site/settings.py
+++ leprikon/site/settings.py
@@ -149,6 +149,18 @@ SOCIAL_AUTH_GOOGLE_PLUS_SCOPE = ['profile', 'email']
 SOCIAL_AUTH_GOOGLE_PLUS_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_PLUS_KEY')
 SOCIAL_AUTH_GOOGLE_PLUS_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_PLUS_SECRET')
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 # Authentication configuration
 AUTHENTICATION_BACKENDS = (
     ['social_core.backends.facebook.FacebookOAuth2'] if SOCIAL_AUTH_FACEBOOK_KEY else []
