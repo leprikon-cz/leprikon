e4aea9c3f53ee53933bef0db323d89f5fb366675
Jakub Dorňák <jakub.dornak@misli.cz>
2019-08-29 11:59:10 +0200

    improve security
    - limit user editing and login-as function to superuser

--- leprikon/admin/user.py
+++ leprikon/admin/user.py
@@ -3,7 +3,7 @@ from django.conf.urls import url as urls_url
 from django.contrib import admin
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.admin import UserAdmin as _UserAdmin
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.contrib.messages import ERROR
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
@@ -65,9 +65,12 @@ class UserAdmin(SendMessageAdminMixin, _UserAdmin):
     merge.short_description = _('Merge selected user accounts')
 
     def get_list_display(self, request):
-        return ['id'] \
-            + list(super(UserAdmin, self).get_list_display(request)) \
-            + ['login_as_link']
+        list_display = ['id'] + list(super(UserAdmin, self).get_list_display(request))
+        if request.user.is_superuser:
+            list_display += ['login_as_link_superuser']
+        elif request.user.has_perm('auth.change_user'):
+            list_display += ['login_as_link']
+        return list_display
 
     def get_search_fields(self, request):
         return list(super(UserAdmin, self).get_search_fields(request)) + [
@@ -79,15 +82,27 @@ class UserAdmin(SendMessageAdminMixin, _UserAdmin):
             'leprikon_participants__email',
         ]
 
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = list(super(UserAdmin, self).get_readonly_fields(self, request, obj)) + [
+            'last_login',
+            'date_joined',
+        ]
+        if not request.user.is_superuser:
+            readonly_fields += ['is_staff', 'is_superuser', 'groups', 'user_permissions']
+        return readonly_fields
+
     def get_urls(self):
         urls = super(UserAdmin, self).get_urls()
-        login_as_view = self.admin_site.admin_view(user_passes_test(lambda u: u.is_superuser)(self.login_as))
+        login_as_view = self.admin_site.admin_view(permission_required('auth.change_user')(self.login_as))
         return [
             urls_url(r'(?P<user_id>\d+)/login-as/$', login_as_view, name='auth_user_login_as'),
         ] + urls
 
     def login_as(self, request, user_id):
-        user = get_object_or_404(get_user_model(), id=user_id)
+        if request.user.is_superuser:
+            user = get_object_or_404(get_user_model(), id=user_id)
+        else:
+            user = get_object_or_404(get_user_model(), id=user_id, is_staff=False, is_superuser=False)
         user.backend = 'django.contrib.auth.backends.ModelBackend'
         login(request, user)
         return HttpResponseRedirect(reverse('leprikon:summary'))
@@ -96,9 +111,17 @@ class UserAdmin(SendMessageAdminMixin, _UserAdmin):
         return '<a href="{url}">{text}</a>'.format(
             url     = reverse('admin:auth_user_login_as', args=[obj.id]),
             text    = _('login')
-        )
+        ) if not obj.is_staff and not obj.is_superuser else ''
     login_as_link.allow_tags = True
     login_as_link.short_description = _('login')
+
+    def login_as_link_superuser(self, obj):
+        return '<a href="{url}">{text}</a>'.format(
+            url     = reverse('admin:auth_user_login_as', args=[obj.id]),
+            text    = _('login')
+        )
+    login_as_link_superuser.allow_tags = True
+    login_as_link_superuser.short_description = _('login')
 
     def get_message_recipients(self, request, queryset):
         return queryset.all()
