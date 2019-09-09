f3d475ca48e902edb648c8bd5c03cc387c4284f7
Jakub Dorňák <jakub.dornak@misli.cz>
2019-09-09 19:18:13 +0200

    prevent legal agreement from being deleted

--- leprikon/admin/agreements.py
+++ leprikon/admin/agreements.py
@@ -16,3 +16,15 @@ class AgreementAdmin(admin.ModelAdmin):
     list_display = ('name', 'order')
     list_editable = ('order',)
     list_filter = (ActiveListFilter,)
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and obj.registrations.exists():
+            return False
+        else:
+            return super().has_delete_permission(request, obj)
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del(actions['delete_selected'])
+        return actions
