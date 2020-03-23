8de8babbd440e267acf86452e8529cd43261ce65
Jakub Dorňák <jakub.dornak@misli.cz>
2020-03-23 09:52:02 +0100

    Add Sentry support to cronjobs

--- leprikon/cronjobs.py
+++ leprikon/cronjobs.py
@@ -4,16 +4,29 @@ from itertools import chain
 from django.conf import settings
 from django.db.models import Q
 from django_cron import CronJobBase, Schedule
+from sentry_sdk import capture_exception
 
 from .models.courses import CourseRegistration
 from .models.events import EventRegistration
 
 
-class SendPaymentRequest(CronJobBase):
+class SentryCronJobBase(CronJobBase):
+    def dojob(self):
+        raise NotImplementedError(f'{self.__class__.__name__}.dojob must be implemented.')
+
+    def do(self):
+        try:
+            return self.dojob()
+        except Exception:
+            capture_exception()
+            raise
+
+
+class SendPaymentRequest(SentryCronJobBase):
     schedule = Schedule(run_at_times=[settings.CRON_SEND_PAYMENT_REQUEST_TIME])
     code = 'leprikon.cronjobs.SendPaymentRequest'
 
-    def do(self):
+    def dojob(self):
         today = date.today()
         for registration in chain(
             CourseRegistration.objects.filter(
