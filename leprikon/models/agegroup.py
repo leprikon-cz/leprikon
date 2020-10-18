77f1d90e26b206c13749f9f4aa5e6da3e8a19c0d
Jakub Dorňák <jakub.dornak@misli.cz>
2020-10-18 09:41:18 +0200

    Add age (soft) limits to age groups

--- leprikon/models/agegroup.py
+++ leprikon/models/agegroup.py
@@ -6,6 +6,8 @@ from ..utils import first_upper
 
 class AgeGroup(models.Model):
     name = models.CharField(_('name'), max_length=50)
+    min_age = models.PositiveSmallIntegerField(_('minimal age'), blank=True, null=True)
+    max_age = models.PositiveSmallIntegerField(_('maximal age'), blank=True, null=True)
     order = models.IntegerField(_('order'), blank=True, default=0)
     require_school = models.BooleanField(_('require school'), default=True)
 
