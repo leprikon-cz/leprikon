5d055f09ba097d4e73549909484eacc9b26c5d55
Jakub Dorňák <jakub.dornak@misli.cz>
2020-10-21 19:55:59 +0200

    Use common manage.py

--- manage.py
+++ manage.py
@@ -1,23 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env python
+import os
+import sys
 
-if ! which leprikon &> /dev/null; then
-    if ! test -e env/bin/activate; then
-        virtualenv env
-    fi
-    . env/bin/activate
-    if ! which leprikon &> /dev/null; then
-        pip install -r requirements.txt
-    fi
-fi
-
-if [ -n "$1" ]; then
-    export DEBUG="${DEBUG:-TEMPLATE}"
-else
-    # bash-completion
-    export DEBUG=""
-fi
-
-export PYTHONPATH=.
-#export SITE_MODULE=leprikon
-
-exec leprikon "$@"
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leprikon.site.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
