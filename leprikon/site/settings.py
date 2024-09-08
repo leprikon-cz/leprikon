108c5fbafb9f4bc9a9ab7f017e2f882bdba63154
Jakub Dorňák <jakub.dornak@qbsoftware.cz>
2024-09-08 16:22:29 +0200

    Update thumbnail processors

--- leprikon/site/settings.py
+++ leprikon/site/settings.py
@@ -448,8 +448,10 @@ THUMBNAIL_DEBUG = "THUMBNAIL" in os.environ.get("DEBUG", "").split(",")
 THUMBNAIL_PROCESSORS = (
     "easy_thumbnails.processors.colorspace",
     "easy_thumbnails.processors.autocrop",
+    # "easy_thumbnails.processors.scale_and_crop",
     "filer.thumbnail_processors.scale_and_crop_with_subject_location",
     "easy_thumbnails.processors.filters",
+    "easy_thumbnails.processors.background",
 )
 
 # Haystack configuration
