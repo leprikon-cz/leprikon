8519bda0291ba024bbebab79fb5a9df9a0d8e3f1
Jakub Dorňák <jakub.dornak@qbsoftware.cz>
2026-06-02 14:50:33 +0200

    prevent built-in HTML5 validation on hidden textarea field

--- leprikon/forms/form.py
+++ leprikon/forms/form.py
@@ -1,3 +1,5 @@
+from djangocms_text.widgets import TextEditorWidget
+
 class FormMixin(object):
     required_css_class = "required"
     use_get = False
@@ -12,3 +14,7 @@ class FormMixin(object):
             # add data-type
             if "data-type" not in self.fields[f].widget.attrs:
                 self.fields[f].widget.attrs["data-type"] = self.fields[f].__class__.__name__
+            # prevent built-in HTML5 validation on hidden textarea field
+            field = self.fields[f]
+            if isinstance(field.widget, TextEditorWidget):
+                field.required = False
